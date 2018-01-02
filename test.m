75bc16154bbd0a7aabe1225d2d0a3ecc2e8653d5
kh8 <reczek.krzysztof@gmail.com>
2018-01-03 00:05:19 +0100

    Functional approach

--- test.m
+++ test.m
@@ -1,13 +1,10 @@
+function test (X1)
+
 X = [1, 29, 1, 1; 1, 32, 1, 1; 1, 45, 2, 1; 1, 47, 2, 1; 1, 45, 3, 1; 1, 56, 3, 1; 1, 72, 4, 2];
-disp ('X matrix: '), disp (X);
-
 Y = [210; 250; 270; 280; 300; 320; 450];
-disp ('Y matrix: '), disp (Y);
-
 Theta = pinv(X'*X)*X'*Y;
-disp ('Theta matrix: '), disp (Theta);
-
-X1 = [1; 50; 2; 1];
 predictedPrice = Theta'*X1;
 
 disp ('X1 matrix: '), disp (X1), disp ('predicted price:'), disp (predictedPrice);
+
+endfunction

137a0a070484423a01e46624c95d4b5905d7d00a
kh8 <reczek.krzysztof@gmail.com>
2018-01-14 15:56:55 +0100

    EX2: Plotting, Sigmoid function and cost function

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,12 +20,8 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
-
-
-
-
+h0 = sigmoid(X * theta);
+J = sum((-1 * y)' *  log(h0) - (1 - y)' * log(1 - h0)) / m;
 
 % =============================================================
 
